85b9a28e1d7188d1441281c6469e3b0808de7c19
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2022-01-08 21:00:04 +0200

    Working: Formular adaugare Localitate

--- frontend/views/localitate/_form.php
+++ frontend/views/localitate/_form.php
@@ -1,5 +1,8 @@
 <?php
 
+use common\models\Judet;
+use kartik\select2\Select2;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
@@ -12,22 +15,70 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'localitate_nume')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'localitate_judet')->textInput() ?>
-
-    <?= $form->field($model, 'localitate_status')->textInput() ?>
-
-    <?= $form->field($model, 'localitate_urban')->textInput() ?>
-
-    <?= $form->field($model, 'localitate_resedinta')->textInput() ?>
-
-    <?= $form->field($model, 'localitate_created')->textInput() ?>
-
-    <?= $form->field($model, 'localitate_updated')->textInput() ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    <div class="container">
+        <div class="row my-2">
+            <div class="col-12 text-center bg-success text-white p-2">
+                <h1>Adaugă Localitate</h1>
+            </div>
+        </div>
+        <div class="row my-2">
+            <div class="col-4">
+                <?= $form->field($model, 'localitate_nume')->textInput(['maxlength' => true]) ?>
+            </div>
+            <div class="col-4">
+                <?= $form->field($model, 'localitate_judet')->widget(Select2::classname(), [
+                    'data' => ArrayHelper::map(Judet::find()->orderBy(['judet_nume' => SORT_ASC])->all(), 'id', 'judet_nume'),
+                    'language' => 'de',
+                    'options' => ['placeholder' => 'Județ ...'],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ],
+                ])
+                ?>
+            </div>
+            <div class="col-4">
+                <?= $form->field($model, 'localitate_urban')->widget(Select2::classname(), [
+                        'data' => ['0' => 'NU', '1' => 'DA'],
+                        'language' => 'de',
+                        'options' => ['placeholder' => 'Urban / Rural ...'],
+                        'pluginOptions' => [
+                            'allowClear' => true
+                        ],
+                    ])
+                ?>
+            </div>
+        </div>
+        <div class="row my-2">
+            <div class="col-4">
+                <?= $form->field($model, 'localitate_resedinta')->widget(Select2::classname(), [
+                        'data' => ['0' => 'NU', '1' => 'DA'],
+                        'language' => 'de',
+                        'options' => ['placeholder' => 'Reședință de județ ...'],
+                        'pluginOptions' => [
+                            'allowClear' => true
+                        ],
+                    ])
+                ?>
+            </div>
+            <div class="col-4">
+                <?= $form->field($model, 'localitate_status')->widget(Select2::classname(), [
+                        'data' => ['0' => 'Inactivă', '1' => 'Activă'],
+                        'language' => 'de',
+                        'options' => ['placeholder' => 'Stare localitate ...'],
+                        'pluginOptions' => [
+                            'allowClear' => true
+                        ],
+                    ])
+                ?>
+            </div>
+        </div>
+        <div class="row my-2">
+            <div class="col-12">
+                <div class="form-group">
+                    <?= Html::submitButton('Salveaza Datele', ['class' => 'btn btn-success btn-block']) ?>
+                </div>
+            </div>
+        </div>
     </div>
 
     <?php ActiveForm::end(); ?>
