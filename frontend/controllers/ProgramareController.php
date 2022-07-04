8f28805ed2de713b4ff5691755ac0e5a8f1a7853
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2022-07-04 22:38:35 +0300

    Lucrator serviciu menu links added - programare

--- frontend/controllers/ProgramareController.php
+++ frontend/controllers/ProgramareController.php
@@ -177,26 +177,6 @@ class ProgramareController extends Controller
     }
 
     /**
-     * Updates an existing Programare model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param int $id ID
-     * @return string|\yii\web\Response
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('update', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
      * Deletes an existing Programare model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
      * @param int $id ID
@@ -228,7 +208,9 @@ class ProgramareController extends Controller
         return $this->redirect(['site/index']);
 
     }
-
+    // =============================================
+    // VALIDARE PROGRAMARE
+    // =============================================
     public function actionValideazaProgramare()
     {
         $dataResponse = [
@@ -774,6 +756,9 @@ class ProgramareController extends Controller
         }
     }
 
+    // ========================================================
+    // ATRIBUIRE PROGRAMARE UNUI LUCRATOR SERVICU
+    // ========================================================
     public function actionAtribuire($id_programare = NULL){
         if(!\Yii::$app->user->getIsGuest() && \Yii::$app->user->can('director_institutie')){
             $programare = Programare::findOne($id_programare);
---
+++ frontend/views/layouts/partials/menus/left_menu/lucrator_serviciu.php
@@ -0,0 +1,15 @@
+<?php
+
+use yii\helpers\Html;
+?>
+<!-- Sidebar Menu -->
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item">
+            <a href="index.php?r=programare" class="nav-link">
+                <i class="fas fa-list-ul nav-icon"></i>
+                <p>Programari</p>
+            </a>
+        </li>
+    </ul>
+</nav>
